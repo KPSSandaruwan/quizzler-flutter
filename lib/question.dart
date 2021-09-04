0df9fd2791462af0914ae64003baa9d0cc864643
KPSSandaruwan <kpsurajasandaruwn@gmail.comm>
2021-09-04 16:30:58 +0530

    Add OOP concepts to enhance the app

---
+++ lib/question.dart
@@ -0,0 +1,14 @@
+class Question {
+
+  // Properties
+  String questionText;
+  bool questionAnswer;
+
+  //Constructor
+  Question(String question, bool answer) {
+    questionText = question;
+    questionAnswer = answer;
+  }
+
+  // If there is a function it become a method of that class
+}
